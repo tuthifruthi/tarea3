5a9f49057b597dfa0e33d48cbc4ca6aa11fdc6ac
tuthifruthi <maria.ramirezco@alumnos.usm.cl>
2014-07-01 21:13:10 -0400

    index, formulario postulación

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<style type="text/css">
+body p {
+	font-family: Verdana, Geneva, sans-serif;
+}
+.a {
+	font-family: Verdana, Geneva, sans-serif;
+}
+</style>
+
+<body>
+<title>Bienvenido</title>
+<big class="a">Bienvenido al sistema</big>
+<br>&nbsp;</br>
+
+<form method="post">
+  <p>ROL USM:
+    <input type="text" name="name"><br>
+    Contrasena: 
+    <input type="text" name="password">
+  </p>
+  <p>
+    <input type="submit" value="Ingresar">
+  </p>
+</form>
+<p>Si quieres ser colaborador...
+<form method= "post" action="postulacion.php">
+  <input type="submit" value="Postula Aqui">
+ </form> 
+</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
---
+++ postulacion.php
@@ -0,0 +1,57 @@
+<html>
+<style type="text/css">
+body p {
+	font-family: Verdana, Geneva, sans-serif;
+}
+.a {
+	font-family: Verdana, Geneva, sans-serif;
+}
+</style>
+
+<body>
+<title>Postulacion</title>
+<big class="a">POSTULACION</big>
+<br>&nbsp;</br>
+
+
+<form method="post"> 
+  <p>Nombre:
+  <input type="text" name="name">
+<br><br>
+    ROL USM: <input type="text" name="rol">
+<br><br>
+    RUT: <input type="text" name="rut">
+<br><br>
+    Carrera: <input type="text" name="carrera">
+<br><br>
+    Correo electronico: 
+    <input type="text" name="email">
+<br><br>
+    Telefono: 
+    <input type="text" name="phonenumber">
+<br><br>
+    Contrasena: 
+    <input type="text" name="password">
+<br><br>
+    Preferencia 1:
+    <select>
+      <option value="pref1" name="pref1">Area</option>
+    </select><br>
+    Preferencia 2:
+    <select>
+      <option value="pref2" name="pref2" >Area</option>
+    </select>
+  <br>
+    Preferencia 3:
+    <select>
+      <option value="pref3" name="pref3" >Area</option>
+    </select>
+  <br>
+&nbsp;  </p>
+  <p>
+    <input type="submit" value="Postular">
+  </p>
+</form>
+
+</body>
+</html>
