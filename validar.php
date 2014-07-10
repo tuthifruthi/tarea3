c6972ceeb7d45176602bea8e8dc865a91ae273ae
mauriciodelrio <mauricio.delrioz@alumnos.usm.cl>
2014-07-10 04:41:44 -0400

    validaciones 1.03
    falta crear los menus colab y postu

--- validar.php
+++ validar.php
@@ -14,8 +14,34 @@
 		$consult=conn->consulta($sql);
 		$row = pg_fetch_array($consult);
 		if ($row["contrasena"]==$pass) {
-			$_SESSION["id_alumno"] = $row["id_alumno"];
-			
+			$_SESSION["rol"] = $row["rol"];
+			$sql3 = 'SELECT "id_coordinador" FROM "coordinadores" WHERE "rol"=\''.$row["rol"].'\'';
+			$consult2= conn->consulta($sql3);
+			if ($row2 = pg_fetch_array($consult2)) {
+				//es coordinador, yay.
+
+				header("Location:menu_coordin.php");
+			}
+
+			else if{
+
+			$sql4 = 'SELECT "id_colaborador" FROM "colaboradores" WHERE "rol"=\''.$row["rol"].'\'';
+			$consult3= conn->consulta($sql4);
+
+				if ($row3 = pg_fetch_array($consult3)) {
+					//es colaborador, yay.
+
+					header("Location:menu_colab.php");
+				}
+
+				else{
+
+					//es postulante
+					header("Location:menu_postu.php");
+				}
+
+			}
+
 			
 		}
 
